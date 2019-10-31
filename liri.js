7d66e89bf37e2797cb370dd4fe009fc1eba15a88
B Kelly <berenicekelly808@gmail.com>
2019-10-30 18:27:40 -0700

    commands working

--- liri.js
+++ liri.js
@@ -4,74 +4,193 @@ var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
-// file system for do-what-it-says function/command
-var fs = require("fs");
+// axios npm for concert-this and movie-this functions/commands
+var axios = require("axios");
 
 // moments npm for concert-this function/command
 var moment = require("moment");
 
-// axios npm for concert-this and movie-this functions/commands
-var axios = require("axios");
+// file system for do-what-it-says function/command
+var fs = require("fs");
 
-var command = process.argv[2]
-var arguments = process.argv.slice(3).join(" ")
+// CL Arguments
+var args = process.argv;
+var command = args[2];
+var title = args[3];
+console.log("Entered command: " + command);
+console.log("Entered title: " + title);
+console.log("---");
+
+// Switch statement:
 switch (command){
 case 'concert-this':
- console.log('do something with bands in town');
- break;
+  concertThis(title);
+  break;
 case 'spotify-this-song':
-    console.log('do something with spotify');
+    spotifyThis(title);
     break;
 case 'movie-this':
-    console.log('do something with omdb');
+    movieThis(title);
     break;
 case 'do-what-it-says':
-    console.log ('will go to spotify');
-    break;
+  // read file
+  fs.readFile('./random.txt', 'utf8', function(error, data){
+  if(error){
+                  return console.log(error);
+              }
+              var textCommand = data.split(",");
+              var fCommand = textCommand[0];
+              var fTitle;
+              
+              if(textCommand[1] !== undefined){
+// Remove double quotes from title in text file
+                  fTitle = textCommand[1].replace(/['"]+/g, '');
+              }
+  
+              console.log("Text Command: " + fCommand);
+              console.log("Text Title: " + fTitle);
+switch(fCommand){
+  case 'concert-this':
+      concertThis(fTitle);
+      break;
+  case 'spotify-this-song':
+      spotifyThis(fTitle);
+      break;
+  case 'movie-this':
+      movieThis(fTitle);
+      break;
+  case 'do-what-it-says':
+      console.log("We don't want any loops now...");
+      break;
+  default:
+      console.log("Unknown command: " + fCommand);
+}
+  });
+break;
+case 'help':
+console.log("COMMANDS:");
+console.log("concert-this '<band>': Shows concert info for a given band");
+console.log("spotify-this-song '<song-name>': Shows song information from Spotify");
+console.log("movie-this '<movie-title>': Shows movie information for the given title");
+console.log("do-what-it-says: Executes command in the 'random.txt' file (format: <command>,\"<title>\")");
+break;
+default:
+console.log("Unknown command (type 'help' for list of commands)");
 }
 
-function concert(inputs) {
-  var queryUrl =
-    "https://rest.bandsintown.com/artists/" +
-    inputs +
-    "/events?app_id=codingbootcamp";
+function concertThis(artist){
+if(artist !== undefined){
+var query = artist.split(" ").join("+").split('.').join("");
+console.log("Query: " + query);
+axios.get("https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp")
+.then(function(response){
+// console.log(response);
+var concerts = response.data;
 
-}
-function findSong(song){
-  spotify.search({ type: 'track', query: arguments})
-   .then(function(response) {
-     console.log(response);
-   })
-   .catch(function(err) {
-     console.log(err);
-   });
-}
-function findMovie(movie){
- var URL = 'http://www.omdbapi.com/?apikey=trilogy&'
-}
-function findDo (command){
-  switch (action) {
-    case "spotify-this-song":
-      spotify(inputs);
-      break;
+console.log("Type of concerts: " + typeof concerts);
+if(typeof concerts === "object"){
+  if(concerts.length > 0){
+      console.log("Concerts ==============");
+      concerts.forEach(function(concert, i){
+          var venue = concert.venue;
+          console.log("CONCERT " + i + ":");
+          console.log("Venue: " + venue.name);
+          console.log("Location: " + venue.city + ", " + venue.region);
+          console.log("Date: " + moment(concert.datetime).format("MM/DD/YYYY"));
+          console.log("+++++");
+      });
+  } else{
+      console.log("No concerts found.");
+  }
   
-    case "movie-this":
-      movie(inputs);
-      break;
-  
-    case "concert-this":
-      concert(inputs);
-      break;
-  
-    case "do-what-it-says":
-      doIt(inputs);
-      break;
+} else{
+  console.log("Artist not found...");
+}
+
+console.log("**********************");
+});
+} else {
+console.log("Please enter an artist");
+}
+}
+
+function spotifyThis(title){
+if(title === undefined){
+title = 'the sign ace of base';
+}
+spotify.search({ type: 'track', query: title }, function(error, data){
+if(error){
+return console.log("Error occurred: " + error);
+}
+
+// console.log(data);
+if(data.tracks.items.length > 0){
+// Uses first result
+var track = data.tracks.items[0];
+
+// Track Artists
+var artistsObj = track.artists;
+var artists = [];
+artistsObj.forEach(function(artist){
+  // console.log(artist.name);
+  if(artist.type === "artist"){
+      artists.push(artist.name);
+  }
+});
+// console.log(JSON.stringify(track, null, 8));
+
+var song = track.name;
+var preview = track.preview_url;
+var album = track.album.name;
+
+//Output to user
+console.log("Artists: " + artists.join(", "));
+console.log("Song Name: " + song);
+console.log("Preview Link: " + preview);
+console.log("Album: " + album);
+console.log("**********************");
+} else{
+console.log("Song not found");
+}
+
+});
+}
+
+function movieThis(title){
+if(title === undefined){
+title = 'Mr. Nobody';
+}
+var query = title.split(" ").join("+").split('.').join("");
+axios.get("http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy").
+then(function(response) {
+
+if(response.data.Error){
+return console.log(response.data.Error);
+} else{
+// console.log(response);
+console.log("Title: " + response.data.Title);
+console.log("Year: " + response.data.Year);
+console.log("IMDB Rating: " + response.data.imdbRating);
+
+//Ratings
+var ratings = response.data.Ratings
+var tomatoRating = "N/A";
+if(typeof ratings === "object"){
+  for(key in ratings){
+      var source = ratings[key].Source;
+      if(source === "Rotten Tomatoes"){
+          tomatoRating = ratings[key].Value;
+      }
   }
 }
 
-//Function to read the random.txt file
-function doIt(inputs) {
-  fs.readFile("random.txt", "utf-8", function(err, buf) {
-    console.log(buf.toString());
-  });
+console.log("Rotten Tomatoes Rating: " + tomatoRating);
+
+console.log("Country: " + response.data.Country);
+console.log("Language: " + response.data.Language);
+console.log("Plot: " + response.data.Plot);
+console.log("Actors: " + response.data.Actors);
+}
+console.log("**********************");
+});
 }
